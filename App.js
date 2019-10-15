9c97c2306cb6983503e75c9ba81057db75eadf2a
iDmitriy-dev <dmitriy.p86@ya.ru>
2019-10-15 23:17:43 +0600

    cart added

--- App.js
+++ App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font'
 
 import productReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
 import ShopNavigator from './navigation/ShopNavigator';
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  })
+};
+
 const rootReducer = combineReducers({
-  products: productReducer
+  products: productReducer,
+  cart: cartReducer
 });
 
 const store = createStore(rootReducer);
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+  if (!fontLoaded) {
+    return <AppLoading startAsync={fetchFonts} onFinish={() => {
+      setFontLoaded(true);
+    }} />
+  }
   return (
     <Provider store={store}>
       <ShopNavigator></ShopNavigator>
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { FlatList, Text } from 'react-native';
-import { useSelector } from 'react-redux';
+import { FlatList, Platform } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import ProductItem from '../../components/shop/ProductItem';
+import * as cartActions from '../../store/actions/cart';
+import HeaderButton from '../../components/UI/HeaderButton';
 
 const ProductsOverviewScreen = props => {
    const products = useSelector(state => state.products.availableProducts);
+   const dispatch = useDispatch();
+
    return (
       <FlatList
          data={products}
@@ -15,15 +20,39 @@ const ProductsOverviewScreen = props => {
                image={itemData.item.imageUrl}
                title={itemData.item.title}
                price={itemData.item.price}
-               onViewDetail={() => { }}
-               onAddToCart={() => { }}
+               onViewDetail={() => {
+                  props.navigation.navigate({
+                     routeName: 'ProductDetail',
+                     params: {
+                        productId: itemData.item.id,
+                        productTitle: itemData.item.title
+                     }
+                  })
+                  // alternative navigate('ProductDetail', {productId: itemData.item.id})
+               }}
+               onAddToCart={() => {
+                  dispatch(cartActions.addToCart(itemData.item))
+               }}
             />
          }
       />)
 };
 
-ProductsOverviewScreen.navigationOptions = {
-   headerTitle: 'All products'
+ProductsOverviewScreen.navigationOptions = navData => {
+   return {
+      headerTitle: 'All products',
+      headerRight: (
+         <HeaderButtons HeaderButtonComponent={HeaderButton}>
+            <Item
+               title='Cart'
+               iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+               onPress={() => {
+                  navData.navigation.navigate('Cart')
+               }}
+            />
+         </HeaderButtons>
+      )
+   };
 }
 
 export default ProductsOverviewScreen;
